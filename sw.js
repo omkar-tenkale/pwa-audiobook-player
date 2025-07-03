4b51f1492d19aa9935ecd1815f14b129588be6e9
omkar-tenkale <omkar-tenkale@users.noreply.github.com>
2025-07-03 19:35:17 +0000

    deploy: bd57f6505f2347ef186c6f4fee2531fef8dac38b

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 
         const ASSETS = [
-  "assets/index.1b86d9a6.js",
+  "assets/index.94c5adfb.js",
   "assets/disable-app-if-not-supported.9b4340fe.js",
-  "assets/view-artists.12633be3.js",
-  "assets/modal.4379af9d.js",
-  "assets/add-to-playlist-modal.7c534b87.js",
-  "assets/create-or-rename-playlist.a03ea769.js",
+  "assets/view-artists.67ed7727.js",
+  "assets/modal.2a7c0f3a.js",
+  "assets/add-to-playlist-modal.b82665fa.js",
+  "assets/create-or-rename-playlist.cc11afee.js",
   "assets/app-theme.c2072629.js",
   "assets/tracks-file-parser-worker.9dc4a2d2.js",
-  "assets/player.a9f28fef.js",
-  "assets/details.6ebc89f8.js",
-  "assets/search.06887d75.js",
-  "assets/settings.e0e84aa1.js",
-  "assets/about.8e724552.js",
-  "assets/not-found.c09c2f20.js",
+  "assets/player.99171b47.js",
+  "assets/details.44084e8e.js",
+  "assets/search.1d10f45b.js",
+  "assets/settings.ef5a56bc.js",
+  "assets/about.5a7cc315.js",
+  "assets/not-found.40dc1d1f.js",
   "assets/style.ad889449.css",
   "assets/va9E4kDNxMZdWfMOD5VvmojLeTY.c042fdbc.woff2",
   "assets/va9E4kDNxMZdWfMOD5Vvk4jLeTY.75f38e9c.woff2",
@@ -29,13 +29,13 @@
   "assets/va9B4kDNxMZdWfMOD5VnZKveSRf6TF0.460d60e3.woff2",
   "assets/va9B4kDNxMZdWfMOD5VnZKveSBf6TF0.53126e5f.woff2",
   "assets/va9B4kDNxMZdWfMOD5VnZKveRhf6.d7e96d52.woff2",
-  "assets/webfonts.152ace54.css",
+  "assets/webfonts.0d46cfcb.css",
   "index.html",
   "manifest.webmanifest",
   "/icons/icon_responsive.svg",
   "/icons/icon_maskable.svg"
 ];
-        const VERSION = "a89b697dbbfe60265741917763777fedcb6989cb";
+        const VERSION = "d8d77438c56df126444fccad631789f9e02fcdb1";
         self.addEventListener("fetch",(e=>{e.respondWith((async()=>{const{request:s}=e,t=new URL(s.url);if("GET"===s.method&&self.location.origin===t.origin){const e="navigate"===s.mode?"/index.html":s;return caches.match(e).then((e=>e||fetch(s)))}return fetch(s)})())})),self.addEventListener("install",(e=>{const s=caches.open(VERSION).then((e=>e.addAll(ASSETS)));e.waitUntil(s)})),self.addEventListener("activate",(e=>{self.clients.claim();const s=caches.keys().then((e=>{const s=e.map((e=>{if(e!==VERSION)return caches.delete(e)}));return Promise.all(s)}));e.waitUntil(s)})),self.addEventListener("message",(e=>{"skip-waiting"===e.data&&self.skipWaiting()}));
 
       
