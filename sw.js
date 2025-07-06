23101a1fba1d044c601dcdb2853df5aa8ede9f3a
omkar-tenkale <omkar-tenkale@users.noreply.github.com>
2025-07-06 18:28:54 +0000

    deploy: 65b2740297ea7909e09ad4bb994314d9c977ed99

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
 
         const ASSETS = [
-  "assets/index.e7521a95.js",
+  "assets/index.6b900303.js",
   "assets/disable-app-if-not-supported.9b4340fe.js",
-  "assets/view-artists.9f603c33.js",
-  "assets/modal.47cbba38.js",
-  "assets/add-to-playlist-modal.d0b27057.js",
-  "assets/create-or-rename-playlist.de1dbccb.js",
+  "assets/view-artists.e7f4b83e.js",
+  "assets/modal.d0b41a6e.js",
+  "assets/add-to-playlist-modal.95e02932.js",
+  "assets/create-or-rename-playlist.017a8b3a.js",
   "assets/app-theme.c2072629.js",
   "assets/tracks-file-parser-worker.9dc4a2d2.js",
-  "assets/player.8537303c.js",
-  "assets/details.c9a64bdd.js",
-  "assets/search.dee7efc6.js",
-  "assets/settings.cf9c042b.js",
-  "assets/about.c64a75f0.js",
-  "assets/not-found.d5ebcd4e.js",
+  "assets/player.2820fb64.js",
+  "assets/details.f1e901a7.js",
+  "assets/search.5428921b.js",
+  "assets/settings.9892b3de.js",
+  "assets/about.b06741a6.js",
+  "assets/not-found.557a3702.js",
   "assets/style.4c7eba61.css",
   "index.html",
   "manifest.webmanifest",
   "/icons/icon_responsive.svg",
   "/icons/icon_maskable.svg"
 ];
-        const VERSION = "6732bf4f002580f3b50ea53aa0a65c1148b20261";
+        const VERSION = "4abf96e246262d27b653557f540178c9f7b8f113";
         self.addEventListener("fetch",(e=>{e.respondWith((async()=>{const{request:s}=e,t=new URL(s.url);if("GET"===s.method&&self.location.origin===t.origin){const e="navigate"===s.mode?"/index.html":s;return caches.match(e).then((e=>e||fetch(s)))}return fetch(s)})())})),self.addEventListener("install",(e=>{const s=caches.open(VERSION).then((e=>e.addAll(ASSETS)));e.waitUntil(s)})),self.addEventListener("activate",(e=>{self.clients.claim();const s=caches.keys().then((e=>{const s=e.map((e=>{if(e!==VERSION)return caches.delete(e)}));return Promise.all(s)}));e.waitUntil(s)})),self.addEventListener("message",(e=>{"skip-waiting"===e.data&&self.skipWaiting()}));
 
       
