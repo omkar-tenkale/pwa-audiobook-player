85ee545a56a721013bd70ef720031ed01e05207e
omkar-tenkale <omkar-tenkale@users.noreply.github.com>
2025-07-06 12:34:32 +0000

    deploy: a599ecaa709a358432f580c4248a24fef68754cb

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 
         const ASSETS = [
-  "assets/index.94c5adfb.js",
+  "assets/index.6457234f.js",
   "assets/disable-app-if-not-supported.9b4340fe.js",
-  "assets/view-artists.67ed7727.js",
-  "assets/modal.2a7c0f3a.js",
-  "assets/add-to-playlist-modal.b82665fa.js",
-  "assets/create-or-rename-playlist.cc11afee.js",
+  "assets/view-artists.6c17e910.js",
+  "assets/modal.650243e2.js",
+  "assets/add-to-playlist-modal.09af54a2.js",
+  "assets/create-or-rename-playlist.ff469f92.js",
   "assets/app-theme.c2072629.js",
   "assets/tracks-file-parser-worker.9dc4a2d2.js",
-  "assets/player.99171b47.js",
-  "assets/details.44084e8e.js",
-  "assets/search.1d10f45b.js",
-  "assets/settings.ef5a56bc.js",
-  "assets/about.5a7cc315.js",
-  "assets/not-found.40dc1d1f.js",
+  "assets/player.64cc6bf4.js",
+  "assets/details.0454f837.js",
+  "assets/search.e0352bfc.js",
+  "assets/settings.8c94770e.js",
+  "assets/about.759c4845.js",
+  "assets/not-found.daf091cb.js",
   "assets/style.ad889449.css",
   "assets/va9E4kDNxMZdWfMOD5VvmojLeTY.c042fdbc.woff2",
   "assets/va9E4kDNxMZdWfMOD5Vvk4jLeTY.75f38e9c.woff2",
@@ -35,7 +35,7 @@
   "/icons/icon_responsive.svg",
   "/icons/icon_maskable.svg"
 ];
-        const VERSION = "446131f6f6654990a03f8c0e91949e3c2d28a3ea";
+        const VERSION = "c25d05c5584082e6276039695647d99c4379114d";
         self.addEventListener("fetch",(e=>{e.respondWith((async()=>{const{request:s}=e,t=new URL(s.url);if("GET"===s.method&&self.location.origin===t.origin){const e="navigate"===s.mode?"/index.html":s;return caches.match(e).then((e=>e||fetch(s)))}return fetch(s)})())})),self.addEventListener("install",(e=>{const s=caches.open(VERSION).then((e=>e.addAll(ASSETS)));e.waitUntil(s)})),self.addEventListener("activate",(e=>{self.clients.claim();const s=caches.keys().then((e=>{const s=e.map((e=>{if(e!==VERSION)return caches.delete(e)}));return Promise.all(s)}));e.waitUntil(s)})),self.addEventListener("message",(e=>{"skip-waiting"===e.data&&self.skipWaiting()}));
 
       
