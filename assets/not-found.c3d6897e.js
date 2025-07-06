cbaa9b9bb8493eb26675ff4d417d1b817bfeea88
omkar-tenkale <omkar-tenkale@users.noreply.github.com>
2025-07-06 16:08:40 +0000

    deploy: 6930a421e286284f4dfb9a93db4961d9bd02fd93

---
+++ assets/not-found.c3d6897e.js
@@ -0,0 +1,1 @@
+import{c as e,M as t,y as o}from"./index.e2dc2028.js";const r=()=>e(o,{title:"Not found",topBar:!1,get children(){return e(t,{title:"Nothing here :(",message:"Page you were looking for was not found",button:{href:"/",title:"Go Home"}})}});export{r as default};
